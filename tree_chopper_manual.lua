63247a4466481b8c14709a2b1f40e1523e74b1bf
Sam Davy <samdavy@protonmail.com>
2024-07-21 13:47:20 -0600

    test

--- tree_chopper_manual.lua
+++ tree_chopper_manual.lua
@@ -2,7 +2,7 @@ function equip()
   for i = 1, 16 do
     tool_slot = 1
     curr_item = turtle.getItemDetail(i).name
-    if curr_item == "minecraft:diamond_axe" || curr_item == "minecraft:diamond_pickaxe" || curr_item == "minecraft:diamond_shovel" || curr_item == "minecraft:diamond_hoe" || curr_item == "minecraft:diamond_sword" then
+    if curr_item == "minecraft:diamond_axe" or curr_item or "minecraft:diamond_pickaxe" or curr_item or "minecraft:diamond_shovel" or curr_item == "minecraft:diamond_hoe" or curr_item == "minecraft:diamond_sword" then
       tool_slot = i
       turtle.equipLeft(tool_slot)
       return 0
