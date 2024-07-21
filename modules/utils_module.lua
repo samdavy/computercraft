88a3cc43d5c5edd6720a4a3a3b22c5830f3885cd
Sam Davy <samdavy@protonmail.com>
2024-07-21 14:19:50 -0600

    i

--- modules/utils_module.lua
+++ modules/utils_module.lua
@@ -1,16 +1,13 @@
--- utils_module.lua
-
--- Create a table to hold the module's functions and variables
 local utils = {}
-
 
 function utils.equipTool()
     for i=1, 16 do
-      item = turtle.getItemDetail(i).name
-      -- check if its any diamond tool and equip if it is, else return false
-      if item == "minecraft:diamond_pickaxe" or item == "minecraft:diamond_axe" or item == "minecraft:diamond_shovel" or item == "minecraft:diamond_sword" or item == "minecraft:diamond_hoe" then
-        turtle.select(i)
-        return true
+      if turtle.getItemDetail(i) then 
+        item = turtle.getItemDetail(i).name
+        if item == "minecraft:diamond_pickaxe" or item == "minecraft:diamond_axe" or item == "minecraft:diamond_shovel" or item == "minecraft:diamond_sword" or item == "minecraft:diamond_hoe" then
+          turtle.select(i)
+          return true
+        end
       end
     end
 end
